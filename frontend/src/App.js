98f9d6fc0fe6a03a707f77bd137cb56c5e41508f
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-05-21 23:30:26 +0530

    File strcuture and refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,190 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
+import {BrowserRouter,Route,Link} from "react-router-dom"
+//components
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   const openMenu = () => {
-    document.querySelector(".sidebar").classList.add("open");
+    document.querySelector('.sidebar').classList.add('open');
   };
   const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open");
-  }
+    document.querySelector('.sidebar').classList.remove('open');
+  };
 
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">Zodex-Herbals</a>
-        </div>
-        <div className="header-links">
-          <a href="signin.html">Sign-In</a>
-          <a href="cart.html">Cart</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          x
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">Floor Cleaners</a>
-          </li>
-          <li>
-            <a href="index.html">Bathroom Cleaners</a>
-          </li>
-          <li>
-            <a href="index.html">Herbal Cleaners</a>
-          </li>
-          <li>
-            <a href="index.html">Cloth Cleaners</a>
-          </li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Zodex-Herbal</Link>
+          </div>
+          <div className="header-links">
+            <a href="signin.html">Sign-In</a>
+            <a href="cart.html">Cart</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            x
+          </button>
+          <ul>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">Floor Cleaners</a>
             </li>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">Bathroom Cleaners</a>
             </li>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">Herbal Cleaners</a>
             </li>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">Cloth Cleaners</a>
             </li>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">Black-Phenyl</a>
             </li>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">White-Phenyl</a>
             </li>
             <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="phenyl image"
-                ></img>
-                <div className="product-name">
-                  <a href="product.html">Zodex Phenyl 1</a>
-                </div>
-                <div className="product-brand">Zodex</div>
-                <div className="product-price">100 Rs</div>
-                <div className="product-rating">4.5 Stars(10 Reviews)</div>
-              </div>
+              <a href="index.html">Bleaching-Powders</a>
             </li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">All rights reserved by Zodex -Herbals</footer>
-    </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products/:id" component={ProductScreen}/>
+            <Route path="/" exact={true} component={HomeScreen} />
+            
+          </div>
+        </main>
+        <footer className="footer">
+          All rights reserved by Zodex-Herbals
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+export default {
+    products:[
+    {
+        _id:"1",
+        name:"Zodex Phenyl",
+       category:"Bathroom Cleaners",
+        image:"/images/d1.jpg",
+        price:60,
+        brand:"Zodex",
+        rating:4.8,
+        numReviews:10
+    },{
+        _id:"2",
+        name:"Zodex Phenyl",
+       category:"Bathroom Cleaners",
+        image:"/images/d1.jpg",
+        price:60,
+        brand:"Zodex",
+        rating:4.8,
+        numReviews:10
+    },{
+        _id:"3",
+        name:"Zodex Phenyl",
+       category:"Bathroom Cleaners",
+        image:"/images/d1.jpg",
+        price:60,
+        brand:"Zodex",
+        rating:4.8,
+        numReviews:10
+    },{
+        _id:"4",
+        name:"Zodex Phenyl",
+       category:"Bathroom Cleaners",
+        image:"/images/d1.jpg",
+        price:60,
+        brand:"Zodex",
+        rating:4.8,
+        numReviews:10
+    },{
+        _id:"5",
+        name:"Zodex Phenyl",
+       category:"Bathroom Cleaners",
+        image:"/images/d1.jpg",
+        price:60,
+        brand:"Zodex",
+        rating:4.8,
+        numReviews:10
+    }]
+}
